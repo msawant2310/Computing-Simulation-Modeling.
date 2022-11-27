c3f5aefc809910f79f3eb862751f46e7ff61121f
msawant2310 <113842002+msawant2310@users.noreply.github.com>
2022-11-26 20:30:44 -0500

    Delete Question8.py

--- Question8.py
+++
@@ -1,9 +0,0 @@
-#Q.8
-class anagram:
-    def ana(self,s,t):
-        if set(s)==set(t) and len(s)==len(t):
-            return True
-        else:
-            return False
-        
-anagram().ana("Avinash","ivshanA")
