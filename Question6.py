8143cd0a55d7917bc5a8aa39a6c098d8ddeccc5d
msawant2310 <113842002+msawant2310@users.noreply.github.com>
2022-11-26 20:30:33 -0500

    Delete Question6.py

--- Question6.py
+++
@@ -1,8 +0,0 @@
-#Q.6
-import cv2
-import matplotlib.pyplot as plt
-
-img = cv2.imread('/Users/mrunalisawant/Downloads/20220917_111622.jpg')
-plt.imshow(img)
-bilateral_img = cv2.bilateralFilter(img, 15, 75, 75)
-plt.imshow(bilateral_img)
